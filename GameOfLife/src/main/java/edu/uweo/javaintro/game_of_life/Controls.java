046defcb20b174c8b7bafc06ce6b1411f3d161bb
John W. Straub <jstraub@uw.edu>
2018-08-01 15:57:03 -0700

    Roll in latest changes

--- GameOfLife/src/main/java/edu/uweo/javaintro/game_of_life/Controls.java
+++ GameOfLife/src/main/java/edu/uweo/javaintro/game_of_life/Controls.java
@@ -6,16 +6,45 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 public class Controls
 	implements Runnable
 {
-	private final JFrame	frame	= new JFrame( "Game of Life Controller" );
+	public static final String RUN_LABEL           = "Run";
+	public static final String INTERACTIVE_LABEL   = "Interactive";
+	public static final String STOP_LABEL          = "Stop";
+	public static final String EXIT_LABEL          = "Exit";
+	public static final String STEP_LABEL          = "Step";
+	public static final String CLEAR_LABEL         = "Clear";
+	public static final String OPEN_LABEL          = "Open";
+	public static final String SAVE_LABEL          = "Save";
+    public static final String GPS_LABEL           = "Generations per Second";
+    public static final String MAX_GPS_LABEL       = "Max Generations per Second";
+	
+	private String[]   userButtons     = { "", "", "" };
+	
+	private final JFrame       frame       = new JFrame( "Game of Life Controller" );
+	private final MainPanel    mainPanel   = new MainPanel();
+	private final JSlider      slider      = new JSlider();
+	private final JTextField   maxGPSText  = new JTextField();
+	private final JLabel       maxGPLabel  = new JLabel( MAX_GPS_LABEL );
+	
 	private final List<ControlListener>	controlListeners	= new ArrayList<>();
+	
+	public static void main( String[] args )
+	{
+		new Controls().start();
+	}
 	
 	public void start()
 	{
@@ -72,5 +101,49 @@ public class Controls
 			for ( ControlListener listener : controlListeners )
 				listener.controlActivated( evt );
 		}
+	}
+	
+	private class WestPanel extends JPanel
+	{
+	    public WestPanel()
+	    {
+    	    AbstractButton[]   controls    =
+            {
+                new JCheckBox( INTERACTIVE_LABEL ),
+                new JButton( EXIT_LABEL ),
+                new JButton( OPEN_LABEL ),
+                new JButton( SAVE_LABEL ),
+            };
+    	    
+    	    for ( AbstractButton button : controls )
+    	    {
+    	        button.addActionListener( mainPanel );
+    	        add( button );
+    	    }
+    	    
+            for ( String str : userButtons )
+            {
+                JButton button  = new JButton( str );
+                button.addActionListener( mainPanel );
+                add( button );
+            }
+            
+            int     rows        = this.getComponentCount();
+            setLayout( new GridLayout( rows, 1) );
+	    }
+	}
+	
+	private class CenterPanel extends JPanel
+	{
+	    public CenterPanel()
+	    {
+	        JPanel maxGPSpanel = new JPanel();
+	        maxGPSpanel.add( new JLabel( MAX_GPS_LABEL ) );
+	        JComponent[]   components  =
+	            {
+                    new JLabel( GPS_LABEL ),
+                    slider,
+	            };
+	    }
 	}
 }
